c3b3aacbeb5f5cc3a7e540172299d49abf22d51c
Yurasynor <Yurasynor@gmail.com>
2020-05-21 00:06:50 +0300

    add hw_1_1

---
+++ Homework_1_1.py
@@ -0,0 +1,7 @@
+
+a = int(float(input(" введіть число 1:\n")))
+b = int(float(input(" введіть число 2:\n")))
+print(a, '+', b, '=', a+b)
+print(a, '-', b, '=', a-b)
+print(a, '*', b, '=', a*b)
+print(a, '/', b, '=', a/b)
