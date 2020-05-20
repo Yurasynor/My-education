b7ecc9d58f608cbbce7f483b30df26efde614231
Yurasynor <Yurasynor@gmail.com>
2020-05-21 00:12:35 +0300

    add hw_1_2

---
+++ Homework_1_2.py
@@ -0,0 +1,5 @@
+name = input ("What is your name?")
+age = input ("How old are you?")
+location = input ("Where do you live?")
+print("Hello " + name + ", your age is " + age + ", you live in  " + location)
+
